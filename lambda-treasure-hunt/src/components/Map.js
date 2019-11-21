1791be5db492faf094d2e1398d374c76d2ddb94b
Adam Mathieson <mathieson1222@gmail.com>
2019-11-20 21:06:19 -0800

    refactor and add more value as variables

--- lambda-treasure-hunt/src/components/Map.js
+++ lambda-treasure-hunt/src/components/Map.js
@@ -2,21 +2,30 @@ import React from 'react'
 
 const Map = ({prev, room, dirToPrev}) => {
     console.log("dirToPrev ?? : ", dirToPrev)
-    // const graphMap = {0: {"n": "?", "s": "?", "e": "?", "w": "?"}}
-    // localStorage.setItem('graphMap', JSON.stringify(graphMap))
+    // Check if graphMap has been added to Local Storage
+    if (!localStorage.getItem('graphMap')) {
+
+        const graphMap = {0: {"n": "?", "s": "?", "e": "?", "w": "?"}}
+
+        localStorage.setItem('graphMap', JSON.stringify(graphMap))
+    } 
 
     const graphMap = JSON.parse(localStorage.getItem('graphMap'))
-    let keys = Object.keys(graphMap)
+    const keys = Object.keys(graphMap)
+    const strID = JSON.stringify(room.room_id)
+    console.log("strID: ",strID)
 
+    // If you enter another room log the previous room obj to corresponding direction
     if (room.room_id !== undefined && prev.room_id !== undefined) {
-        if (keys.includes(JSON.stringify(room.room_id))) {
-            const value = graphMap[JSON.stringify(room.room_id)][dirToPrev]
+        if (keys.includes(strID)) {
+            const value = graphMap[strID][dirToPrev]
             if (value === '?')
-            graphMap[JSON.stringify(room.room_id)][dirToPrev] = JSON.stringify(prev.room_id)
+            graphMap[strID][dirToPrev] = JSON.stringify(prev)
             localStorage.setItem('graphMap', JSON.stringify(graphMap))
             console.log("room is in graph", value)
+        // If it's not there add it to the object
+        } else if (!keys.includes(strID)) {
 
-        } else {
             console.log("room NOT in graph", room)
         }
     } else {
