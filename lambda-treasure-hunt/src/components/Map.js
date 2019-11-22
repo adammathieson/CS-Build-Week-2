fbc9be8e79957bdd1b57277e1a9f7aca7ff43553
Adam Mathieson <mathieson1222@gmail.com>
2019-11-22 09:48:07 -0800

    adds a 2D array to Map, need to get the actual room objects and not just the ids

--- lambda-treasure-hunt/src/components/Map.js
+++ lambda-treasure-hunt/src/components/Map.js
@@ -4,22 +4,38 @@ import './Map.css'
 
 const Map = () => {
 
-const dummyData = []
-let count = 4900
-while (count  > 0) {
-    dummyData.push(count)
-    count--
+const twoDMap = new Array(70)
+for (let i = 0; i < twoDMap.length; i++) {
+    twoDMap[i] = new Array(70)
 }
 
-console.log(dummyData.length)
+if (localStorage.getItem('graphMap')) {
+    const graphMap = JSON.parse(localStorage.getItem('graphMap'))
+    console.log(graphMap)
+    const keys = Object.keys(graphMap)
+    console.log(keys.length)
+}
+
+
+
+
+// twoDMap.forEach(item => {
+//     let count = 70
+//     while (count < 0) {
+//         item.push('🟢')
+//         count--
+//     }
+// })
+
+twoDMap.map(item => console.log(item.length))
 
     return (
         <div>
             <h2>The Map</h2>
             <div className="grid-container">
-                {dummyData.map(item => (
+                {/* {twoDMap.map(item => (
                     <div key={item}>🟢</div>
-                ))}
+                ))} */}
             </div>
             
         </div>
